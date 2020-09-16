14478072a813161f190e71df96d53ce626b30527
Son Hoang <haitac865@gmail.com>
2020-09-16 13:50:54 +0700

    commit

--- Compare Tool/Form1.Designer.cs
+++ Compare Tool/Form1.Designer.cs
@@ -28,45 +28,137 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.log = new System.Windows.Forms.RichTextBox();
+            this.folderO = new System.Windows.Forms.ListView();
+            this.folderT = new System.Windows.Forms.ListView();
+            this.btnCompare = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnb = new System.Windows.Forms.Button();
+            this.btnb1 = new System.Windows.Forms.Button();
+            this.btnDump = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // button1
+            // log
             // 
-            this.button1.Location = new System.Drawing.Point(86, 176);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(127, 52);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.log.Location = new System.Drawing.Point(12, 206);
+            this.log.Name = "log";
+            this.log.Size = new System.Drawing.Size(644, 227);
+            this.log.TabIndex = 0;
+            this.log.Text = "";
             // 
-            // button2
+            // folderO
             // 
-            this.button2.Location = new System.Drawing.Point(392, 176);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.folderO.HideSelection = false;
+            this.folderO.Location = new System.Drawing.Point(12, 47);
+            this.folderO.Name = "folderO";
+            this.folderO.Size = new System.Drawing.Size(280, 140);
+            this.folderO.TabIndex = 1;
+            this.folderO.UseCompatibleStateImageBehavior = false;
+            // 
+            // folderT
+            // 
+            this.folderT.HideSelection = false;
+            this.folderT.Location = new System.Drawing.Point(376, 47);
+            this.folderT.Name = "folderT";
+            this.folderT.Size = new System.Drawing.Size(280, 140);
+            this.folderT.TabIndex = 2;
+            this.folderT.UseCompatibleStateImageBehavior = false;
+            // 
+            // btnCompare
+            // 
+            this.btnCompare.Location = new System.Drawing.Point(298, 80);
+            this.btnCompare.Name = "btnCompare";
+            this.btnCompare.Size = new System.Drawing.Size(72, 64);
+            this.btnCompare.TabIndex = 3;
+            this.btnCompare.Text = "Compare";
+            this.btnCompare.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(230, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(376, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(230, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // btnb
+            // 
+            this.btnb.Location = new System.Drawing.Point(248, 10);
+            this.btnb.Name = "btnb";
+            this.btnb.Size = new System.Drawing.Size(43, 23);
+            this.btnb.TabIndex = 6;
+            this.btnb.Text = "...";
+            this.btnb.UseVisualStyleBackColor = true;
+            // 
+            // btnb1
+            // 
+            this.btnb1.Location = new System.Drawing.Point(612, 10);
+            this.btnb1.Name = "btnb1";
+            this.btnb1.Size = new System.Drawing.Size(44, 23);
+            this.btnb1.TabIndex = 7;
+            this.btnb1.Text = "...";
+            this.btnb1.UseVisualStyleBackColor = true;
+            // 
+            // btnDump
+            // 
+            this.btnDump.Location = new System.Drawing.Point(581, 443);
+            this.btnDump.Name = "btnDump";
+            this.btnDump.Size = new System.Drawing.Size(75, 23);
+            this.btnDump.TabIndex = 8;
+            this.btnDump.Text = "Dump";
+            this.btnDump.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 190);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Logs";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(668, 363);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(668, 478);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnDump);
+            this.Controls.Add(this.btnb1);
+            this.Controls.Add(this.btnb);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnCompare);
+            this.Controls.Add(this.folderT);
+            this.Controls.Add(this.folderO);
+            this.Controls.Add(this.log);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Compare Tool";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox log;
+        private System.Windows.Forms.ListView folderO;
+        private System.Windows.Forms.ListView folderT;
+        private System.Windows.Forms.Button btnCompare;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnb;
+        private System.Windows.Forms.Button btnb1;
+        private System.Windows.Forms.Button btnDump;
+        private System.Windows.Forms.Label label1;
     }
 }
 
